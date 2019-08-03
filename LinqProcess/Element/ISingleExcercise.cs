cfe043fe666302ae854a6970f16f4a317d0978e2
Jayakumar <Jayakumar@DESKTOP-HF8154Q>
2019-08-04 02:06:44 +0530

    Single operator done.

---
+++ LinqProcess/Element/ISingleExcercise.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqProcess.Element
+{
+    /*
+        The linq method is used to return a single element from data source
+        or we can say from a sequence.        
+            
+        public static Single<TSource>(this IEnumerable<TSource> source) -- No input parameter
+        Note: 
+            If the data source is empty or if the data source contain more then one element, 
+            then it return exception.
+
+        public static Single<TSource>(this IEnumerable<TSource>, Func<Tsource, bool> predicate) -- one predicate parameter
+        Note: It throw an exception while the below condition
+            1. If the data source is empty
+            2. Condition doesn't satesfy any elements in sequence
+            3. Condition satesfies more then one element 
+            
+        Success: 1..1 – one element matches	
+        Thrown Exception: 0 | >1 – elements match 
+        Use: If exactly 1 element is expected; not 0 or more than 1
+    */
+    public interface ISingleExcercise
+    {
+        int SearchFirstMark(int[] marks);
+        int SearchTopMark(int[] marks, int filter);
+        int SearchTopMark(int[] marks, Func<int, bool> predicate);
+    }
+
+    public class SingleExcercise : ISingleExcercise
+    {
+        public int SearchFirstMark(int[] marks)
+        {
+            if (marks == null && marks.Length < 1)
+                throw new InvalidOperationException();
+            return marks.Single();
+        }
+
+        public int SearchTopMark(int[] marks, int filter)
+        {
+            if (marks == null && marks.Length < 1)
+                throw new InvalidOperationException();
+            return marks.Single(x => x > filter);
+        }
+
+        public int SearchTopMark(int[] marks, Func<int, bool> predicate)
+        {
+            if (marks == null && marks.Length < 1)
+                throw new InvalidOperationException();
+            return marks.Single(predicate);
+        }
+    }
+}
---
+++ LinqProcessTest/Element/SingleExcerciseTest.cs
@@ -0,0 +1,73 @@
+﻿using LinqProcess.Element;
+using NUnit.Framework;
+using System;
+
+namespace LinqProcessTest.Element
+{
+    [TestFixture]
+    public class SingleExcerciseTest
+    {
+        private int[] mathMarks = null;
+        private ISingleExcercise _singleExcercise = null;
+        [SetUp]
+        public void Initilize()
+        {
+            mathMarks = new int[] { 34, 56, 78, 98, 65, 45, 34, 22, 99, 56
+                                    , 89, 34, 78, 88, 94 };
+            _singleExcercise = new SingleExcercise();
+        }
+
+        [Test]
+        public void Check_SingleExcercise_Got_Memory()
+        {
+            // Arrange // Act // Assert
+            Assert.IsNotNull(_singleExcercise);
+        }
+        [Test]
+        public void Check_The_Object_Type()
+        {
+            // Arrange // Act // Assert
+            Assert.IsInstanceOf(typeof(SingleExcercise), _singleExcercise);
+        }
+
+        [Test]
+        public void Check_The_Method_Return_Exception_for_Null()
+        {
+            // Arrange // Act // Assert
+            Assert.Throws<InvalidOperationException>(() => _singleExcercise.SearchFirstMark(new int[] { }));
+        }
+
+        [Test]
+        public void Check_The_Method_Return_Exception_for_MoreThenOne()
+        {
+            // Arrange // Act // Assert
+            Assert.Throws<InvalidOperationException>(() => _singleExcercise.SearchFirstMark(mathMarks));
+        }
+
+        [Test]
+        public void Check_The_Method_Return_10()
+        {
+            // Arrange // Act // Assert
+            var actual = _singleExcercise.SearchFirstMark(new int[] { 10});
+            Assert.AreEqual(10, actual);
+        }
+
+        [Test]
+        public void Check_The_Method_Return_For_Condition()
+        {
+            // Arrange // Act // Assert
+            var actual = _singleExcercise.SearchTopMark(mathMarks, 98);
+            Assert.AreEqual(99, actual);
+        }
+
+        [Test]
+        public void Check_The_Method_Return_For_Predicate()
+        {
+            // Arrange // Act // Assert   
+            Func<int, bool> isGreater =
+            (int x) => x > 98;
+            var actual = _singleExcercise.SearchTopMark(mathMarks, isGreater);
+            Assert.AreEqual(99, actual);
+        }
+    }
+}
