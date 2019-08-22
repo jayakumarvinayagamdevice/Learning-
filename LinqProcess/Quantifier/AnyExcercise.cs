69bbede8b4680d6e4592077544b185889a25cf55
Jayakumar <Jayakumar@DESKTOP-HF8154Q>
2019-08-22 23:58:19 +0530

    Any added.

---
+++ LinqProcess/Quantifier/AnyExcercise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqProcess.Quantifier
+{
+    public interface IAnyExcercise
+    {
+        bool CheckAnyItems(int[] marks);
+        bool CheckAnyItems(int[] marks, int filter);
+        bool CheckAnyItems(int[] marks, Func<int, bool> predicate);
+    }
+    public class AnyExcercise : IAnyExcercise
+    {
+        public bool CheckAnyItems(int[] marks)
+        {
+            if (marks == null)
+                throw new InvalidOperationException();            
+            return marks.Any();
+        }
+
+        public bool CheckAnyItems(int[] marks, int filter)
+        {
+            if (marks == null)
+                throw new InvalidOperationException();
+            return marks.Any(x=> x == filter);
+        }
+
+        public bool CheckAnyItems(int[] marks, Func<int, bool> predicate)
+        {
+            if (marks == null)
+                throw new InvalidOperationException();
+            return marks.Any(predicate);
+        }
+    }
+}
---
+++ LinqProcessTest/Quantifier/AnyExcerciseTest.cs
@@ -0,0 +1,78 @@
+﻿using LinqProcess.Quantifier;
+using NUnit.Framework;
+using System;
+
+namespace LinqProcessTest.Quantifier
+{
+    [TestFixture]
+    public class AnyExcerciseTest
+    {
+        private IAnyExcercise _anyExcercise = null;
+        [SetUp]
+        public void Initilize()
+        {
+            _anyExcercise = new AnyExcercise();
+        }
+        [Test]
+        public void Check_AnyExcercise_Got_Memory()
+        {
+            // Arrange // Act // Assert
+            Assert.IsNotNull(_anyExcercise);
+        }
+        [Test]
+        public void Check_The_Object_Type()
+        {
+            // Arrange // Act // Assert
+            Assert.IsInstanceOf(typeof(AnyExcercise), _anyExcercise);
+        }
+
+        [Test]
+        public void Check_The_Method_Return_for_Null()
+        {
+            // Arrange // Act // Assert
+            Assert.AreEqual(false, _anyExcercise.CheckAnyItems(new int[] { }));
+        }
+        [Test]
+        public void Check_The_Method_For_Empty()
+        {
+            var actual = _anyExcercise.CheckAnyItems(new int[] { });
+            Assert.AreEqual(false, actual);
+        }
+        [Test]
+        public void Check_The_Method_Return_True()
+        {
+            var actual = _anyExcercise.CheckAnyItems(new int[] { 10 });
+            Assert.AreEqual(true, actual);
+        }
+
+        [Test]
+        public void Check_The_Method_for_45()
+        {
+            var actual = _anyExcercise.CheckAnyItems(new int[] { 10, 55, 67, 89, 34, 45, 66, 89 }, 45);
+            Assert.AreEqual(true, actual);
+        }
+
+        [Test]
+        public void Check_The_Method_for_55()
+        {
+            var actual = _anyExcercise.CheckAnyItems(new int[] { 10, 67, 89, 34, 45, 66, 89 }, 55);
+            Assert.AreEqual(false, actual);
+        }
+
+        [Test]
+        public void Check_The_Method_for_Greater_AnyFail()
+        {            
+            Func<int, bool> _isGreater = (int x) => x < 45;
+            var actual = _anyExcercise.CheckAnyItems(new int[] { 10, 67, 89, 34, 45, 66, 89 }, _isGreater);
+            Assert.AreEqual(true, actual);
+        }
+
+        [Test]
+        public void Check_The_Method_for_Greater_AnyPass()
+        {
+            Func<int, bool> _isGreater = (int x) => x > 45;
+            var actual = _anyExcercise.CheckAnyItems(new int[] { 10, 67, 89, 34, 45, 66, 89 }, _isGreater);
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
